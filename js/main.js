970eb4845d7ead1b0f45cc596165497e22a08b6d
AdamReszka <adam.reszka85@gmail.com>
2017-06-07 01:20:46 +0200

    add 3d parallax

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ $(document).ready(initAll);
 
 function initAll(){
   setHeight();
-
+  startParallax();
 }
 
 function setHeight(){
   $('#sec1-main-cont').height($(window).innerHeight());
+}
+
+function startParallax(){
+  $(window).stellar();
 }
